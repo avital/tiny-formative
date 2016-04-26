284716e3aedfc9266745aa7cdf257af25abd2c7a
Avital Oliver <avital@theWE.net>
2016-04-26 16:32:40 -0700

    Delay writing to mongo; use batchInsert

--- common/methods.js
+++ common/methods.js
@@ -1,17 +1,35 @@
 var start = new Date;
 var data = [];
+
+var docsToInsert = [];
+
+Meteor.setInterval(function() {
+  if (docsToInsert.length === 0)
+    return;
+
+  // Copy `docsToInsert` so that while `batchInsert` yields below we
+  // don't lose docs
+  var docs = docsToInsert;
+  docsToInsert = [];
+
+  console.log("Inserting " + docs.length + " docs");
+  Segments.batchInsert(docs);
+}, 5000);
+
 Meteor.methods({
   clear: function() {
     Segments.remove({});
   },
   draw: function(fakeUserId, segments) {
     this.unblock();
-//    data.push({
-//      time: new Date - start,
-//      path: path
-//    });
-//    console.log(JSON.stringify(data));
-    //    console.log();
-    return Segments.insert({fakeUserId, segments});
+    var id = Random.id();
+    var doc = {_id: id, fakeUserId, segments};
+    if (Meteor.isServer) {
+      docsToInsert.push(doc);
+      insertDocInAllActive(doc);
+    } else {
+      Docs.insert(doc);
+    }
+    return id;
   }
 });
